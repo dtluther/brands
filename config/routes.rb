076ecb9b2f10b06aced049a834dc6aa15a65194a
Dillon <dillontluther@gmail.com>
2017-11-17 03:58:53 -0800

    products views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get 'products/create'
-
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/destroy'
-
-  get 'products/update'
+  
 
   root "static_pages#root"
 
@@ -17,6 +9,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
     resources :brands, except: [:new, :edit]
+    resources :products, except: [:new, :edit]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
