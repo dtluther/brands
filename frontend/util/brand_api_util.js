603ad54405910985755afc008228c1587f1a2145
Dillon <dillontluther@gmail.com>
2017-11-16 22:27:42 -0800

    brand api util

---
+++ frontend/util/brand_api_util.js
@@ -0,0 +1,36 @@
+export const fetchBrands = () => {
+    return $.ajax({
+        method: 'GET',
+        url: 'api/brands'
+    });
+};
+
+export const fetchBrand = id => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/brands/${id}`
+    });
+};
+
+export const createBrand = brand => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/brands',
+        data: brand
+    });
+};
+
+export const updateBrand = brand => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `api/brands/${brand.id}`,
+        data: brand
+    });
+};
+
+export const deleteBrand = id => (
+    $.ajax({
+        method: 'DELETE',
+        url: `api/brands/${id}`
+    })
+);
