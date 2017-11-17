6af04e3fda058431d4f7928a4b1661c68de87a7b
Dillon <dillontluther@gmail.com>
2017-11-16 23:35:01 -0800

    entities reducer

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import brandsReducer from './brands_reducer';
+
+const entitiesReducer = combineReducers({
+    brands: brandsReducer,
+    // products: productsReducer
+});
+
+export default entitiesReducer;
